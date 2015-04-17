c66141b913ba22bc8fc169bdd61188506473ff95
苗高亮 <miaogaoliang@kuark.com>
2015-04-17 15:33:54 +0800

    Initial Commit

---
+++ QRCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRCode
+//
+//  Created by kaxiaoer on 15/4/17.
+//  Copyright (c) 2015年 miaogaoliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
